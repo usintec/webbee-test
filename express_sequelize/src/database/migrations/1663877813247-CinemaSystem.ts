329965a27d4d697bc1d4131df77ba7a4722e4f7e
tayo <useful.kyo@gmail.com>
2023-01-16 08:40:57 +0100

    task 4 completed

--- express_sequelize/src/database/migrations/1663877813247-CinemaSystem.ts
+++ express_sequelize/src/database/migrations/1663877813247-CinemaSystem.ts
@@ -1,40 +1,217 @@
-import { QueryInterface } from 'sequelize';
+import { literal, QueryInterface } from 'sequelize';
+import { ModelAttributes } from 'sequelize/types/model';
 
 export default {
-  /**
-   # ToDo: Create a migration that creates all tables for the following user stories
-
-   For an example on how a UI for an api using this might look like, please try to book a show at https://in.bookmyshow.com/.
-   To not introduce additional complexity, please consider only one cinema.
-
-   Please list the tables that you would create including keys, foreign keys and attributes that are required by the user stories.
-
-   ## User Stories
-
-   **Movie exploration**
-   * As a user I want to see which films can be watched and at what times
-   * As a user I want to only see the shows which are not booked out
-
-   **Show administration**
-   * As a cinema owner I want to run different films at different times
-   * As a cinema owner I want to run multiple films at the same time in different showrooms
-
-   **Pricing**
-   * As a cinema owner I want to get paid differently per show
-   * As a cinema owner I want to give different seat types a percentage premium, for example 50 % more for vip seat
-
-   **Seating**
-   * As a user I want to book a seat
-   * As a user I want to book a vip seat/couple seat/super vip/whatever
-   * As a user I want to see which seats are still available
-   * As a user I want to know where I'm sitting on my ticket
-   * As a cinema owner I don't want to configure the seating for every show
-   */
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  up: (queryInterface: QueryInterface): Promise<void> => {
-    throw new Error('TODO: implement migration in task 4');
-  },
+  up: async (queryInterface: QueryInterface): Promise<void> => {
+    await queryInterface.createTable('users', {
+      id: {
+        type: 'integer',
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      firstName: { type: 'varchar' },
+      lastName: {
+        type: 'varchar',
+        allowNull: true,
+      },
+      email: {
+        type: 'email',
+        allowNull: false
+      },
+      password: {
+        type: 'varchar',
+        allowNull: false,
+      },
+      createdAt: {
+        type: 'timestamp',
+        defaultValue: literal('CURRENT_TIMESTAMP'),
+      },
+      updatedAt: {
+        type: 'timestamp',
+        defaultValue: literal('CURRENT_TIMESTAMP'),
+      },
+    } as ModelAttributes);
 
+    await queryInterface.createTable('movie', {
+      id: {
+        type: 'integer',
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      title: {
+        type: 'varchar',
+        allowNull: false,
+      },
+      synopsis: {
+        type: 'varchar',
+        allowNull: true
+      },
+      totalTicket: {
+        type: 'integer',
+        allowNull: true,
+        defaultValue: 0
+      },
+      price: {
+        type: 'decimal',
+        allowNull: false,
+      },
+      createdAt: {
+        type: 'timestamp',
+        defaultValue: literal('CURRENT_TIMESTAMP'),
+      },
+      updatedAt: {
+        type: 'timestamp',
+        defaultValue: literal('CURRENT_TIMESTAMP'),
+      },
+    } as ModelAttributes);
+    await queryInterface.createTable('showrooms', {
+      id: {
+        type: 'integer',
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      name: {
+        type: 'varchar',
+        allowNull: false
+      },
+      totalSeat: {
+        type: 'integer',
+        allowNull: true,
+        defaultValue: 0
+      },
+      totalRowSeat: {
+        type: 'integer',
+        allowNull: false,
+      },
+      totalColumnSeat: {
+        type: 'integer',
+        allowNull: false,
+      },
+      totalVipSeat: {
+        type: 'integer',
+        allowNull: true,
+        defaultValue: 0
+      },
+      createdAt: {
+        type: 'timestamp',
+        defaultValue: literal('CURRENT_TIMESTAMP'),
+      },
+      updatedAt: {
+        type: 'timestamp',
+        defaultValue: literal('CURRENT_TIMESTAMP'),
+      },
+    } as ModelAttributes);
+
+    await queryInterface.createTable('movie-time-table', {
+      id: {
+        type: 'integer',
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      movieId: {
+        type: 'integer',
+        allowNull: false,
+        references: {
+          model: {
+            tableName: 'movie'
+          },
+          key: 'id'
+        },
+        onDelete: 'cascade'
+      },
+      date: {
+        type: 'date',
+        allowNull: false
+      },
+      start: {
+        type: 'timestamp',
+      },
+      end: {
+        type: 'timestamp',
+      },
+      createdAt: {
+        type: 'timestamp',
+        defaultValue: literal('CURRENT_TIMESTAMP'),
+      },
+      updatedAt: {
+        type: 'timestamp',
+        defaultValue: literal('CURRENT_TIMESTAMP'),
+      },
+    } as ModelAttributes);
+
+    await queryInterface.createTable('showroom-time-table', {
+      id: {
+        type: 'integer',
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      movieTimeTableId: {
+        type: 'integer',
+        allowNull: false,
+        references: {
+          model: {
+            tableName: 'movie-time-table'
+          },
+          key: 'id',
+        },
+        onDelete: 'cascade'
+      },
+      showroomId: {
+        type: 'integer',
+        allowNull: false,
+        references: {
+          model: {
+            tableName: 'showroom'
+          },
+          key: 'id'
+        },
+        onDelete: 'cascade'
+      }
+    } as ModelAttributes);
+
+    await queryInterface.createTable('booking', {
+      id: {
+        type: 'integer',
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      userId: {
+        type: 'integer',
+        allowNull: false,
+        references: {
+          model: {
+            tableName: 'user'
+          },
+          key: 'id'
+        }
+      },
+      movieTimeTableId: {
+        type: 'integer',
+        allowNull: false,
+        references: {
+          model: {
+            tableName: 'movie-time-table'
+          },
+          key: 'id',
+        },
+        onDelete: 'cascade'
+      },
+      vipSeatId: {
+        type: ''
+      },
+      vipType: {
+        type: 'varchar',
+        allowNull: true,
+      },
+      vip: {
+        type: 'boolean',
+        defaultValue: false,
+        allowNull: true
+      }
+    } as ModelAttributes)
+  },
+  
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   down: (queryInterface: QueryInterface) => {
     // do nothing
